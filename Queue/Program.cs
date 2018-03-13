32c069e96a6cd90631ac66a297f9b43ed05929d4
Вадим Шванов <adm.shwan@gmail.com>
2018-03-13 14:57:13 +0300

    Add project files.

---
+++ Queue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем новую очередь.
+            var queue = new Queue<int>();
+
+            // Добавляем новые элементы в очередь.
+            queue.Enqueue(1);
+            queue.Enqueue(7);
+            queue.Enqueue(42);
+            queue.Enqueue(69);
+            queue.Enqueue(-17);
+            Console.WriteLine($"Очередь содержит {queue.Count} элементов.");
+
+            // Получаем элементы с удалением.
+            var item1 = queue.Dequeue();
+            Console.WriteLine($"Первый элемент из очереди {item1}.");
+            var item2 = queue.Dequeue();
+            Console.WriteLine($"Второй элемент из очереди {item2}.");
+
+            // Добавляем новый элемент в очередь.
+            queue.Enqueue(88);
+
+            // Просматриваем элемент без удаления.
+            var item3 = queue.Peek();
+            Console.WriteLine($"Обзор элемента без удаления {item3}.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queue
+{
+    /// <summary>
+    /// Очередь.
+    /// </summary>
+    /// <typeparam name="T"> Тип данных, хранимых в очереди. </typeparam>
+    public class Queue<T>
+    {
+        /// <summary>
+        /// Коллекция хранимых данных.
+        /// </summary>
+        private List<T> _items = new List<T>();
+
+        /// <summary>
+        /// Количество элементов.
+        /// </summary>
+        public int Count => _items.Count;
+
+        /// <summary>
+        /// Добавить элемент в очередь.
+        /// </summary>
+        /// <param name="item"> Добавляемые данные. </param>
+        public void Enqueue(T item)
+        {
+            // Проверяем входные данные на пустоту.
+            if(item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            // Добавляем данные в коллекцию элементов.
+            _items.Add(item);
+        }
+
+        /// <summary>
+        /// Получить элемент из очереди с удалением.
+        /// </summary>
+        /// <returns> Элемент данных. </returns>
+        public T Dequeue()
+        {
+            // Получить элемент из начала очереди.
+            var item = GetItem();
+
+            // Удаляем элемент из коллекции.
+            _items.Remove(item);
+
+            // Возвращаем полученный элемент.
+            return item;
+        }
+
+        /// <summary>
+        /// Прочитать элемент из очереди без удаления.
+        /// </summary>
+        /// <returns> Элемент данных. </returns>
+        public T Peek()
+        {
+            // Получаем элемент из начала очереди.
+            var item = GetItem();
+
+            // Возвращаем полученных элемент.
+            return item;
+        }
+
+        /// <summary>
+        /// Получить элемент из начала очереди.
+        /// </summary>
+        /// <returns> Начальный элемент очереди. </returns>
+        private T GetItem()
+        {
+            // Получаем первый элемент.
+            var item = _items.FirstOrDefault();
+
+            // Если элемент пуст, то сообщаем об ошибке.
+            if (item == null)
+            {
+                throw new NullReferenceException("Очередь пуста. Нет элементов для получения.");
+            }
+
+            return item;
+        }
+    }
+}
